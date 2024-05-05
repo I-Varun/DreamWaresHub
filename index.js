f45962b5565b2cafebde6ee580875d1cfaa5db5e
Varun Singh <varunsingh04092001@gmail.com>
2024-05-06 00:19:36 +0530

    Testing backend

--- index.js
+++ index.js
@@ -166,7 +166,7 @@ const stripe = require("stripe")(process.env.STRIPE_SERVER_KEY);
 
 
 server.post("/create-payment-intent", async (req, res) => {
-  const { totalAmount } = req.body;
+  const { totalAmount , orderId} = req.body;
 
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
@@ -176,6 +176,9 @@ server.post("/create-payment-intent", async (req, res) => {
     automatic_payment_methods: {
       enabled: true,
     },
+    metadata:{
+      orderId
+    }
   });
 
   res.send({
