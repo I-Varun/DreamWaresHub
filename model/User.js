0adcaa964239ee46d8e6d22c145e133bced870fa
Varun Singh <varunsingh04092001@gmail.com>
2024-05-11 15:02:15 +0530

    updated logout/timestamp/orderemail/product and cart schema

--- model/User.js
+++ model/User.js
@@ -8,8 +8,9 @@ const userSchema = new Schema({
   addresses: { type: [Schema.Types.Mixed] }, 
   // TODO:  We can make a separate Schema for this
   name: { type: String },
-  salt: Buffer
-});
+  salt: Buffer,
+  resetPasswordToken: {type: String, default:''}
+}, { timestamps: true });
 
 const virtual = userSchema.virtual('id');
 virtual.get(function () {
